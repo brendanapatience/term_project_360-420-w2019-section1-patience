e482f8532f7653cfed60a18d575d413ca8274a25
sosey7701 <patience_brendan@hotmail.com>
2019-05-16 00:32:38 +0000

    haha

--- bin/DoublePendulum.java
+++ bin/DoublePendulum.java
@@ -4,28 +4,33 @@ import javax.swing.*;
 public class DoublePendulum extends JPanel implements Runnable {
 
 	//constants
-  private int L = 100;             		    //length of rod
-  private double M1 = 1;            			//mass of inner bob
-  private double M2 = 1;              		//mass of outer bob
-	private double G = 9.81;					//force of gravity
-	private double dt = 0.01;					//step height
+  private int L = 100;             		//length of rod (cm)
+  private double M1 = 1;            	//mass of inner bob (kg)
+  private double M2 = 1;              //mass of outer bob (kg)
+	private double G = 9.81;					  //force of gravity (N/kg)
+	private double DT = 0.01;					  //step height (s)
+  private boolean difSol = true;      //differential equation solver: true for fourth order runge kutta, false for euler's
 
 	//variables
-  private double t1 = Math.PI / 2;  			//initial angle of inner bob
-  private double t2 = Math.PI;   			//initial angle of outer bob
-	private double w1 = 0;						//initial angular velocity of inner bob
-	private double w2 = 0;						//initial angular velocity of outer bob
-	private double p1;
-	private double p2;
-  private double p1dot;             //is this truly necessary
-  private double p2dot;             //is this truly necessary
+  private double t1 = Math.PI /2;  	 	//initial angle of inner bob (rad)
+  private double t2 = Math.PI /2;   	//initial angle of outer bob (rad)
+	private double w1 = 0;					   	//initial angular velocity of inner bob (rad/s)
+	private double w2 = 0;						  //initial angular velocity of outer bob (rad/s)
+	private double p1;                  //angular momentum of inner bob (kg*m^2 / s)
+	private double p2;                  //angular momentum of outer bob (kg*m^2 / s)
+  private double p1dot;               //derivative of p1 (N*m)
+  private double p2dot;               //derivative of p2 (N*m)
+
+  private double kinetic;
+  private double potential;
+  private double mechanical;
 
     public DoublePendulum() {
         this.L = L;
         this.M1 = M1;
         this.M2 = M2;
 				this.G = G;
-				this.dt= dt;
+				this.DT= DT;
         this.t1 = t1;
         this.t2 = t2;
 				this.p1 = (M1 + M2) * w1 * L*L + M2 * w2 * L*L * Math.cos(t1-t2);
@@ -34,17 +39,18 @@ public class DoublePendulum extends JPanel implements Runnable {
     }
 
     @Override
+    //method for displaying the objects on the screen
     public void paint(Graphics g) {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(Color.BLACK);
         int anchorX = getWidth() / 2, anchorY = getHeight() / 4;
 
-        //First mass (inner means ball)
+        //First bob
         int innerX = anchorX + (int) (Math.sin(t1) * L);
         int innerY = anchorY + (int) (Math.cos(t1) * L);
 
-        //Second mass
+        //Second bob
         int outerX = innerX + (int) (Math.sin(t2) * L);
         int outerY = innerY + (int) (Math.cos(t2) * L);
 
@@ -56,21 +62,24 @@ public class DoublePendulum extends JPanel implements Runnable {
     }
 
     public void run() {
-        double angleAccel, angleVelocity = 0, dt = 0.1; //remember there's a random dt here
         while (true) {
-            //angleAccel = -9.81 / L * Math.sin(t1);
-            //angleVelocity += angleAccel * dt;
-            //t1 += angleVelocity * dt;
+            if (difSol) {
+                //4th order Runge-Kutta method
+                double newVar[] = rungeKutta();
+                t1 = newVar[0];
+                t2 = newVar[1];
+                p1 = newVar[2];
+                p2 = newVar[3];
+            } else {
+                //Euler's method
+                double newVar[] = euler();
+                t1 = newVar[0];
+                t2 = newVar[1];
+                p1 = newVar[2];
+                p2 = newVar[3];
+            }
 
-						////////////////
-						///in here is you run the rungeKutta method
-            double newVar[] = rungeKutta(1); //the one is the "total time" or something
-            t1 = newVar[0];
-            t2 = newVar[1];
-            p1 = newVar[2];
-            p2 = newVar[3];
-						///////////////
-
+            //function to display the next frame
             repaint();
             try { Thread.sleep(1); } catch (InterruptedException ex) {}
         }
@@ -78,39 +87,32 @@ public class DoublePendulum extends JPanel implements Runnable {
 
     @Override
     public Dimension getPreferredSize() {
+        //set dimensions of the created window
         return new Dimension(500, 400);
     }
 
-		public double[] hamilton(double[] variables) {
+    public double[] euler() {
+        DT = this.DT;
 
-				// are these supposed to actually be declared or nah?
-				double t1 = variables[0];
-				double t2 = variables[1];
-				double p1 = variables[2];
-				double p2 = variables[3];
+        double[] baseVar = {this.t1, this.t2, this.p1, this.p2};
+        double[] newVar = new double[baseVar.length];
 
-				double A1 = (p1 * p2 * Math.sin(t1 - t2)) / (L*L * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
-				double A2 = (M2 * L*L * p1*p1 + (M1 + M2) * L*L * p2*p2 - 2 * M2 * L*L * p1 * p2 * Math.cos(t1-t2)) /
-							(2 * Math.pow(L, 4) * Math.pow((M1 + M2 * Math.pow(Math.sin(t1-t2), 2)), 2));
+        double[] increment = hamilton(baseVar);
 
-				w1 = (p1 * L - p2 * L * Math.cos(t1 - t2)) / (Math.pow(L, 3) * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
-				w2 = (- M2 * L * p1 * Math.cos(t1 - t2) + (M1 + M2) * L * p2) / (Math.pow(L, 3) * M2 * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
-				p1dot = - (M1 + M2) * G * L * Math.sin(t1) - A1 + A2 * Math.sin(2*(t1-t2));
-				p2dot = - M2 * G * L * Math.sin(t2) + A1 - A2 * Math.sin(2*(t1-t2));
+        for (int i = 0; i < baseVar.length; i++) {
+            newVar[i] = baseVar[i] + DT * increment[i];
+        }
 
-				double[] hamVar = {w1, w2, p1dot, p2dot};
-				return hamVar;
-		}
+        return newVar;
+    }
 
-		//is xn the total time?
-		//ynplus1 will actually be an array at this rate
-		public double[] rungeKutta(double xn) {
-				dt = this.dt; //I really hope this works
+    public double[] rungeKutta() {
+        DT = this.DT;
 
         double[] baseVar = {this.t1, this.t2, this.p1, this.p2}; //initial variables (yn)
-        double[] interVar = new double[baseVar.length];
+        double[] interVar = new double[baseVar.length];          //intermediate variables
         double[] newVar = new double[baseVar.length];            //updated variables (yn+1)
-        double[][] k = new double[4][baseVar.length];
+        double[][] k = new double[4][baseVar.length];            //2d array containing the 4 k's of each variable
 
         k[0] = hamilton(baseVar);
 
@@ -123,21 +125,46 @@ public class DoublePendulum extends JPanel implements Runnable {
         interVar = intermediate(baseVar, k[2], 1);
         k[3] = hamilton(interVar);
 
+        //calculates the updated variables
         for (int i = 0; i < baseVar.length; i++) {
-          newVar[i] = baseVar[i] + dt * (k[0][i]/6 + k[1][i]/3 + k[2][i]/3 + k[3][i]/6);
+            newVar[i] = baseVar[i] + DT * (k[0][i]/6 + k[1][i]/3 + k[2][i]/3 + k[3][i]/6);
         }
 
         return newVar;
+    }
+
+    //this function is an intermediate step to be able to manipulate k in the rungeKutta method
+    public double[] intermediate(double[] base, double[] k, double div) {
+        DT = this.DT;
+
+        double[] inter = new double[base.length];
+        for (int i = 0; i < base.length; i++) {
+            inter[i] = base[i] + (k[i] * DT) / div;
+        }
+        return inter;
 		}
 
-    public double[] intermediate(double[] base, double[] k, double div) {
-      dt = this.dt;   //again, I hope this works
+    public double[] hamilton(double[] variables) {
 
-      double[] inter = new double[base.length];
-      for (int i = 0; i < base.length; i++) {
-        inter[i] = base[i] + (k[i] * dt) / div;
-      }
-      return inter;
+				double t1 = variables[0];
+				double t2 = variables[1];
+				double p1 = variables[2];
+				double p2 = variables[3];
+
+        //these are sections in the equations that repeat and are therefore clearer in a seperate variable
+				double A1 = (p1 * p2 * Math.sin(t1 - t2)) / (L*L * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
+				double A2 = (M2 * L*L * p1*p1 + (M1 + M2) * L*L * p2*p2 - 2 * M2 * L*L * p1 * p2 * Math.cos(t1-t2)) /
+							(2 * Math.pow(L, 4) * Math.pow((M1 + M2 * Math.pow(Math.sin(t1-t2), 2)), 2));
+
+        //All Hamiltonian equations
+				w1 = (p1 * L - p2 * L * Math.cos(t1 - t2)) / (Math.pow(L, 3) * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
+				w2 = (- M2 * L * p1 * Math.cos(t1 - t2) + (M1 + M2) * L * p2) / (Math.pow(L, 3) * M2 * (M1 + M2 * Math.pow(Math.sin(t1-t2), 2)));
+				p1dot = - (M1 + M2) * G * L * Math.sin(t1) - A1 + A2 * Math.sin(2*(t1-t2));
+				p2dot = - M2 * G * L * Math.sin(t2) + A1 - A2 * Math.sin(2*(t1-t2));
+
+        //array with all the derivatives of the variables
+				double[] hamVar = {w1, w2, p1dot, p2dot};
+				return hamVar;
 		}
 
     public static void main(String[] args) {
